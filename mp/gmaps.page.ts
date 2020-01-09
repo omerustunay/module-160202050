9292d555e1f80d8f1a155bb2017f08d6192f9580
omerustunay <160202050@kocaeli.edu.tr>
2020-01-09 03:35:10 +0300

    google maps uygulaması eklendi.

---
+++ mp/gmaps.page.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LoadingController } from '@ionic/angular';
+
+declare var google;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'gmaps.page.html',
+  styleUrls: ['gmaps.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  mapRef = null;
+
+  constructor(
+    private geolocation: Geolocation,
+    private loadingCtrl: LoadingController
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  async loadMap() {
+    const loading = await this.loadingCtrl.create();
+    loading.present();
+    const myLatLng = await this.getLocation();
+    const mapEle: HTMLElement = document.getElementById('map');
+    this.mapRef = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+    google.maps.event
+    .addListenerOnce(this.mapRef, 'idle', () => {
+      loading.dismiss();
+      this.addMaker(myLatLng.lat, myLatLng.lng);
+    });
+  }
+
+  private addMaker(lat: number, lng: number) {
+    const marker = new google.maps.Marker({
+      position: { lat, lng },
+      map: this.mapRef,
+      title: 'Hello World!'
+    });
+  }
+
+  private async getLocation() {
+    const rta = await this.geolocation.getCurrentPosition();
+    return {
+      lat: 40.766666,
+      lng: 29.916668
+    };
+  }
+
+}
